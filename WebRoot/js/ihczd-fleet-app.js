97f850bc16ca7b08dbfbddb7837a8258fa607efc
lizx <lizx@ihczd.com>
2015-11-23 17:19:04 +0800

    2015-11-23 lizx 修改原生下拉控件
    Change-Id: Ie628e136bb8544e1c0e1cc940b17ee3e26500da3
    Signed-off-by: lizx <lizx@ihczd.com>

--- WebRoot/js/ihczd-fleet-app.js
+++ WebRoot/js/ihczd-fleet-app.js
@@ -1,4 +1,26 @@
 /**
+ * 改良自定义控件
+ **/
+var ihczd_fleet_app = {
+	serializeJsonObjByForm:function(form){//根据表单对象获取表单数据 ,返回类型为json对象
+		var serializeObj={}; 
+		var array=form.serializeArray(); 
+		$(array).each(function(){ 
+			if(serializeObj[this.name]){ 
+				if($.isArray(serializeObj[this.name])){ 
+					serializeObj[this.name].push(this.value); 
+				}else{ 
+					serializeObj[this.name]=[serializeObj[this.name],this.value]; 
+				} 
+			}else{ 
+				serializeObj[this.name]=this.value; 
+			} 
+		}); 
+		return serializeObj; 
+	}
+};
+
+/**
  * 下拉框(对原生的select做了简单的封装)
  * @author lizx
  * @version 1.0
@@ -53,6 +75,7 @@
 				success:function(data){
 					opt.data = data;
 					initOptions($select,opt);
+					this.initFinish = true;
 				},
 				error:function(err){
 					alert('数据加载失败');
@@ -60,8 +83,9 @@
 			});
 		}else{
 			initOptions($select,opt);
+			this.initFinish = true;
 		}
-		
+		//绑定选中事件
 		$select.unbind('change').change(function(e){
 			var selected = $select.find('option:selected')[0];
     		if(opt.onChange && $.isFunction(opt.onChange))
@@ -102,25 +126,62 @@
 			});
 		}
 	};
+	//获取选中值
+	AppSelect.prototype.getValue = function(){
+		var $select = this.$el;
+		var selected = $select.find('option:selected')[0];
+		return selected.value;
+	};
+	//获取选中文本
+	AppSelect.prototype.getText = function(){
+		var $select = this.$el;
+		var selected = $select.find('option:selected')[0];
+		return selected.text;
+	};
+	//根据valueField 字段的值，设置选择
+	AppSelect.prototype.select = function(value){
+		var $this = this;
+		var $select = $this.$el;
+		if($this.options.initFinish){
+			$select.find('option[value="'+value+'"]').attr("selected",true);
+		}
+		else{
+			var int = setInterval(function(){
+				if($this.options.initFinish){
+					clearInterval(int);
+					$select.find('option[value="'+value+'"]').attr("selected",true);
+				}
+			},1000);
+		}
+	};
 	//初始化下拉项
 	function initOptions(el,opt){
-		var $this = el;
-		$this.html('');
-		if(opt.emptyText)
-			$this.append('<option value="">'+opt.emptyText+'</option>');
-		var initValue = $.trim($this.attr('value'));//初始值，select上的value值
-		$.each(opt.data,function(index,obj){
-			if(opt.selText && opt.selText == obj[opt.textField]){//设置指定文本选中，特殊情况使用
-				$this.append('<option value="'+obj[opt.valueField]+'" selected>'+obj[opt.textField]+'</option>');
-				opt.onChange.call(this,obj[opt.valueField],obj[opt.textField],obj);
-			}
-			else if(initValue == obj[opt.valueField]){//值相等是，设置选中
-				$this.append('<option value="'+obj[opt.valueField]+'" selected>'+obj[opt.textField]+'</option>');
-				opt.onChange.call(this,obj[opt.valueField],obj[opt.textField],obj);
-			}else{
-				$this.append('<option value="'+obj[opt.valueField]+'">'+obj[opt.textField]+'</option>');
-			}
-			$this.find('option:last').data('jsonData',obj);
-		});
+		var $select = el;
+		var initValue = $.trim($select.attr('value'));//初始值，select上的value值
+		if(opt.data.length > 0){
+			$select.html('');
+			if(opt.emptyText)
+				$select.prepend('<option value="">'+opt.emptyText+'</option>');
+			$.each(opt.data,function(index,obj){
+				if(opt.selText && opt.selText == obj[opt.textField]){//设置指定文本选中，特殊情况使用
+					$select.append('<option value="'+obj[opt.valueField]+'" selected>'+obj[opt.textField]+'</option>');
+					opt.onChange.call(this,obj[opt.valueField],obj[opt.textField],obj);
+				}
+				else if(initValue == obj[opt.valueField]){//值相等是，设置选中
+					$select.append('<option value="'+obj[opt.valueField]+'" selected>'+obj[opt.textField]+'</option>');
+					opt.onChange.call(this,obj[opt.valueField],obj[opt.textField],obj);
+				}else{
+					$select.append('<option value="'+obj[opt.valueField]+'">'+obj[opt.textField]+'</option>');
+				}
+				$select.find('option:last').data('jsonData',obj);
+			});
+		}else{
+			if(opt.emptyText)
+				$select.prepend('<option value="">'+opt.emptyText+'</option>');
+			$select.find('option[value="'+initValue+'"]').attr("selected",true);
+			var selected = $select.find('option:selected')[0];
+			opt.onChange.call(this,selected.value,selected.text);
+		}
+		opt.initFinish = true;
 	}
 })(jQuery);
