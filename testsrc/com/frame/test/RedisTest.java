acfb4b18ed2ab6eada7eb87c918113ba1f8c1b4f
lizx <lizx@ihczd.com>
2015-11-24 17:30:25 +0800

    2015-11-24 lizx 增加redis操作测试
    Change-Id: I7027484eb637d04109c425337e833fbedd44fdf5
    Signed-off-by: lizx <lizx@ihczd.com>

--- testsrc/com/frame/test/RedisTest.java
+++ testsrc/com/frame/test/RedisTest.java
@@ -1,11 +1,35 @@
 package com.frame.test;
 
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * 
  * @author LiZhiXian
  * @version 1.0
  * @date 2015-11-18 下午5:20:19
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/spring/app-*.xml")
 public class RedisTest {
-
+	
+	@Autowired
+	RedisTemplate<String,String> redisTemplate;
+	@Test
+	public void test(){
+		BoundValueOperations<String, String> boundValueOperations = redisTemplate.boundValueOps("username");
+		//<key类型，对象类型，对象中属性值类型> hset user name "lzx"（分别是这三个的类型）
+		BoundHashOperations<String,String,Object> boundHashOperations = redisTemplate.boundHashOps("user");
+		Map<String, Object> data = boundHashOperations.entries();
+		System.out.println(data);
+		boundValueOperations.set("lsl");
+		System.out.println("-------:"+boundValueOperations.get());
+	}
 }
