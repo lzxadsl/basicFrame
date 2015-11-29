8fc120e30fb7fcbd6c82a015613783fb9bf8f173
lzxadsl <851697971@qq.com>
2015-11-29 17:33:06 +0800

    2015-11-29 lizx 加入了源码包
    Change-Id: Ie020d50d5c2ddfa9f58795401076ec2e1a2afbd4
    Signed-off-by: lzxadsl <851697971@qq.com>

--- src/com/frame/system/interceptor/WebInterceptor1.java
+++ src/com/frame/system/interceptor/WebInterceptor1.java
@@ -4,26 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class WebInterceptor implements HandlerInterceptor{
+public class WebInterceptor1 extends HandlerInterceptorAdapter{
 
 	@Override
 	public void afterCompletion(HttpServletRequest arg0,
 			HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
-		System.out.println("拦截器1——afterCompletion。。。");
+		System.out.println("拦截器2——afterCompletion。。。");
+		
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
 			Object arg2, ModelAndView arg3) throws Exception {
-		System.out.println("拦截器1——postHandle。。。");
+		System.out.println("拦截器2——postHandle。。。");
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
 			Object arg2) throws Exception {
-		System.out.println("拦截器1——preHandle。。。");
+		System.out.println("拦截器2——preHandle。。。");
 		return true;
 	}
 
